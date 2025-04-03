d58066eb50f0efc534c6301c12dc1ef9d629d9d9
echo61201 <alanlu1105@gmail.com>
2025-04-02 23:09:26 -0400

    prerender fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { ItemComponent } from './pages/item/item.component';
 import { FormComponent } from './pages/form/form.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-import { PrerenderRoute } from './PrerenderRoute';
-export const routes: PrerenderRoute[] = [
+
+export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path:'item',component:ItemComponent},
     {path:'form',component:FormComponent},
-    {
-        path: 'product/:id',
-        component: ProductDetailComponent,
-        // so that Vercel won't freak out
-        getPrerenderParams: () => {
-          return [
-            { id: '1' },
-            { id: '2' },
-            { id: '3' }
-          ];
-        }
-      }
-      
+    {path:'product/:id',component:ProductDetailComponent}
 ];
